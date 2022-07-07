5274ec7910f6a6e3cd9b4435cb895b8ffc3e281f
MarkTHamilton <Hamilm25@ferris.edu>
2022-07-07 00:25:47 -0400

    Add files via upload
    "Add expense file"

--- src/Expense.java
+++ src/Expense.java
@@ -1,7 +1,40 @@
 
 public class Expense {
-	String source;
-	double amount;
-	int yearlyfrequency; //1 for 1 time or once a year, 12 for monthly or or 24 for biweekly
-	//should add contructor(s)
+	private String type;
+	private double amount;
+	private int month;
+	
+	public Expense(String type, double amount, int month) {
+		this.type = type;
+		this.amount = amount;
+		this.month = month;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	
+	public String toString() {
+		return type+" $" + String.format("%.2f", amount);
+	}
 }
