f75df9edac62ba5e89e4d64622c5d83830410e64
MarkTHamilton <Hamilm25@ferris.edu>
2022-07-07 00:55:15 -0400

    Add files via upload
    "Load income"

---
+++ src/Income.java
@@ -0,0 +1,41 @@
+
+public class Income {
+	private String type;
+	private double amount;
+	private int month;
+	
+	public Income(String type, double amount, int month) {
+		this.type = type;
+		this.amount = amount;
+		this.month = month;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		this.month = month;
+	}
+	
+	public String toString() {
+		return type+" $" + String.format("%.2f", amount);
+	}
+
+}
